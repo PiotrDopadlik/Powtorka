d046e0ff6e417321e0310e74cc6fc9d8901f8de1
Piotr Dopadlik <sivy.biznes@gmail.com>
2022-09-23 10:55:12 +0200

    w

--- Powtorka/MainWindow.xaml.cs
+++ Powtorka/MainWindow.xaml.cs
@@ -20,17 +20,103 @@ namespace Powtorka
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool czyPani = true;
+        public char pierwszaCyfra; 
         public MainWindow()
         {
             InitializeComponent();
+            pierwszaCyfra = '0';
         }
 
         private void witaj(object sender, RoutedEventArgs e)
         {
-            
             string i = imie.Text;
             string n = nazwisko.Text;
-            MessageBox.Show("Witaj " + i + " " + n);
+            if(sprawdzDate() == false)
+            {
+                MessageBox.Show("Poprawnie wpisz date");
+            }
+            int rokWpisany = int.Parse(rok.Text);
+            char pierwszaCyfra = rok.Text[0];
+            P1.Text = pierwszaCyfra.ToString();
+            if(k.IsChecked == true)
+            {
+                MessageBox.Show("Witaj Pani " + i + " " + n);
+            }else
+            if(m.IsChecked == true)
+            {
+                MessageBox.Show("Witaj Panie " + i + " " + n);
+            }
+        }
+
+        private bool sprawdzDate()
+        {
+            if(rok.Text == ""){
+                return false;
+            }
+            if(miesiac.Text == ""){
+                return false;
+            }
+            if (dzien.Text == ""){
+                return false;
+            }if(rok.Text.Length != 4){
+                return false;
+            }
+            if(Int32.Parse(rok.Text) < 1922 || Int32.Parse(rok.Text) > 2022){
+                return false;
+            }
+            if (miesiac.Text.Length > 2){
+                return false;
+            }
+            if (Int32.Parse(miesiac.Text) < 1 || Int32.Parse(miesiac.Text) > 12){
+                return false;
+            }
+            if (dzien.Text.Length > 2){
+                return false;
+            }
+            if (Int32.Parse(dzien.Text) < 1 || Int32.Parse(dzien.Text) > 31){
+                return false;
+            }
+            return true;
+        }
+
+        private bool sprawdzImie(string imie)
+        {
+            if(imie == "Kuba")
+            {
+                return true;
+            }
+            if (imie[imie.Length-1] == 'a')
+            {
+                return false;
+            }
+            if(imie == "Ines" || imie == "Dolores" || imie == "Inez")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void czKobieta(object sender, RoutedEventArgs e)
+        {
+            if(imie.Text != "")
+            if(sprawdzImie(imie.Text))
+            {
+                MessageBox.Show("Sprawdź czy poprawnie wpisałaś imię");
+                var radio = sender as RadioButton;
+                radio.IsChecked = false;
+            }
+        }
+
+        private void czMeszczyzna(object sender, RoutedEventArgs e)
+        {
+            if(imie.Text != "")
+            if(!sprawdzImie(imie.Text))
+            {
+                MessageBox.Show("Sprawdź czy poprawnie wpisałeś imię");
+                var radio = sender as RadioButton;
+                radio.IsChecked = false;
+            }
         }
     }
 }
